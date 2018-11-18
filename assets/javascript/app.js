492dca2f87273356332bd3eaba5e555a14759085
LRG3 <lrg2013@yahoo.com>
2018-11-17 22:00:55 -0700

    train final

---
+++ assets/javascript/app.js
@@ -0,0 +1,146 @@
+
+var config = {
+	apiKey: "AIzaSyB3e4kRce7bR_rbzA_qlPI6_jfGUhJBXSs",
+    authDomain: "train-schedule-d2ec1.firebaseapp.com",
+    databaseURL: "https://train-schedule-d2ec1.firebaseio.com",
+    projectId: "train-schedule-d2ec1",
+    storageBucket: "train-schedule-d2ec1.appspot.com",
+    messagingSenderId: "737191951128"
+  };
+  
+  firebase.initializeApp(config);
+  
+  
+  var database = firebase.database();
+  
+ 
+  setInterval(function(startTime) {
+	$("#timer").html(moment().format('hh:mm a'))
+  }, 1000);
+  
+ 
+  $("#add-train").on("click", function() {
+	
+	event.preventDefault();
+  
+	
+	var train = $("#trainname-input").val().trim();
+	var destination = $("#destination-input").val().trim();
+	var frequency = $("#frequency-input").val().trim();
+	var firstTime = $("#firsttime-input").val().trim();
+	
+	
+	var trainInfo = { 
+	  formtrain: train,
+	  formdestination: destination,
+	  formfrequency: frequency,
+	  formfirsttime: firstTime,
+	  dateAdded: firebase.database.ServerValue.TIMESTAMP
+	};
+	database.ref().push(trainInfo);
+  
+	console.log(trainInfo.formtrain);
+	console.log(trainInfo.formdestination);
+	console.log(trainInfo.formfrequency);
+	console.log(trainInfo.formfirsttime);
+	console.log(trainInfo.dateAdded);
+  
+	
+	$("#trainname-input").val("");
+	$("#destination-input").val("");
+	$("#frequency-input").val("");
+	$("#firsttime-input").val("");
+  
+  });
+  
+  
+ 
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {  
+	var train = childSnapshot.val().formtrain;
+	var destination = childSnapshot.val().formdestination;
+	var frequency = childSnapshot.val().formfrequency;
+	var firstTime = childSnapshot.val().formfirsttime;
+  
+
+	var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+  
+	
+	var currentTime = moment();
+	console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
+  
+	
+	$("#timer").text(currentTime.format("hh:mm a"));
+  
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+
+	var tRemainder = diffTime % frequency;
+	console.log("Remainder: " + tRemainder);
+  
+	
+	var minutesAway = frequency - tRemainder;
+	console.log("MINUTES TILL TRAIN: " + minutesAway);
+  
+
+	var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+	console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm a"));
+  
+	
+
+	$("#train-table > tbody").append("<tr><td>" + '<i class="fa fa-trash" id="trashcan" aria-hidden="true"></i>' + "</td><td>" + train + "</td><td>" + destination + "</td><td>" +
+	frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+  
+	
+  }, function(errorObject) {
+	console.log("The read failed: " + errorObject.code);
+  });
+  
+  
+ 
+  function timeUpdater() {
+	
+	$("#train-table > tbody").empty();
+	
+	database.ref().on("child_added", function(childSnapshot, prevChildKey) {  
+	var train = childSnapshot.val().formtrain;
+	var destination = childSnapshot.val().formdestination;
+	var frequency = childSnapshot.val().formfrequency;
+	var firstTime = childSnapshot.val().formfirsttime;
+  
+	
+	var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+  
+	
+	var currentTime = moment();
+	console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
+	
+	$("#timer").text(currentTime.format("hh:mm a"));
+
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+
+	var tRemainder = diffTime % frequency;
+	console.log("Remainder: " + tRemainder);
+  
+
+	var minutesAway = frequency - tRemainder;
+	console.log("MINUTES TILL TRAIN: " + minutesAway);
+  
+	
+	var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+	console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm a"));
+  
+ 
+	$("#train-table > tbody").append("<tr><td>" + '<i class="fa fa-trash" aria-hidden="true"></i>' + "</td><td>" + train + "</td><td>" + destination + "</td><td>" +
+	frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+  
+	})
+  };
+  
+  setInterval(timeUpdater, 6000);
+  
+
